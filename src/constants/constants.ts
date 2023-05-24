c4f3787c1cf98991f7eac05e57cbe32077c63789
Tsubasa Katakiri <68495110+TsubasaKatakiri@users.noreply.github.com>
2023-05-24 19:25:17 +0300

    minor changes

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,10 @@
 export const RESULTS_PER_PAGE = 4
+
+export const SECRET_KEY = 'GEU4nvd3rej*jeh.eqp'
+
+export const CLIENT_ID = 2356
+export const CLIENT_SECRET = 'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948'
+
+export const CLIENT_LOGIN = 'sergei.stralenia@gmail.com'
+export const CLIENT_PASSWORD = 'paralect123'
+export const CLIENT_HR = 0
--- src/store/baseApiQuery.ts
+++ src/store/baseApiQuery.ts
@@ -1,11 +1,12 @@
 import { BaseQueryFn, FetchArgs, FetchBaseQueryError, fetchBaseQuery } from '@reduxjs/toolkit/dist/query'
 import { checkAuthorizationTokens, getAccessToken, saveAuthorizationData } from '../helpers/authorizationTokenHelpers'
+import { CLIENT_HR, CLIENT_ID, CLIENT_LOGIN, CLIENT_PASSWORD, CLIENT_SECRET, SECRET_KEY } from '../constants/constants'
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'https://startup-summer-2023-proxy.onrender.com',
     prepareHeaders: (headers) => {
-        headers.set('x-secret-key', 'GEU4nvd3rej*jeh.eqp')
-        headers.set('X-Api-App-Id', 'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948')
+        headers.set('x-secret-key', SECRET_KEY)
+        headers.set('X-Api-App-Id', CLIENT_SECRET)
         headers.set('Authorization', `Bearer ${getAccessToken()}`)
         return headers;
     }
@@ -16,11 +17,11 @@ export const baseQueryWithReauth : BaseQueryFn<string | FetchArgs, unknown, Fetc
     if (result.error && (result.error.status === 401 || result.error.status === 410)) {
         const {access_token, refresh_token} = checkAuthorizationTokens()
         if(refresh_token && !access_token){
-            const refreshResult = await baseQuery(`2.0/oauth2/refresh_token/?refresh_token=${refresh_token}&client_id=2356&client_secret=v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948`, api, extraOptions)
+            const refreshResult = await baseQuery(`2.0/oauth2/refresh_token/?refresh_token=${refresh_token}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`, api, extraOptions)
             saveAuthorizationData(refreshResult.data)
             result = await baseQuery(args, api, extraOptions)
         } else {
-            const loginResult = await baseQuery('/2.0/oauth2/password/?login=sergei.stralenia@gmail.com&password=paralect123&client_id=2356&client_secret=v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948&hr=0', api, extraOptions)
+            const loginResult = await baseQuery(`/2.0/oauth2/password/?login=${CLIENT_LOGIN}&password=${CLIENT_PASSWORD}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&hr=${CLIENT_HR}`, api, extraOptions)
             saveAuthorizationData(loginResult.data)
             result = await baseQuery(args, api, extraOptions)
         }
